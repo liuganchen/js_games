05c528c16fe711fab9c3d876705b8b4f2295872a
liuganchen <liu.ganchen@outlook.com>
2018-02-11 17:54:14 +0800

    修改 css 冲突

--- webapp/appjs/default.js
+++ webapp/appjs/default.js
@@ -10,18 +10,21 @@ const bg_util = (function () {
         console.log(path);
         switch (index) {
             case 1:
+                window.location = path + "/webapp/html/games/fqzs/fqzs.html";
                 break;
             case 2:
+                window.location = path + "/webapp/html/games/2048/2048.html";
                 break;
             case 3:
                 window.location = path + "/webapp/html/games/wuziqi/wuziqi.html";
                 break;
             case 4:
+                window.location = path + "/webapp/html/games/explain.html";
                 break;
             case 5:
                 break;
             case 99:
-                window.location = path + "/webapp/html/index.html";
+                window.location = path + "/webapp/html/fqzs.html";
                 break;
             default:
                 break;
